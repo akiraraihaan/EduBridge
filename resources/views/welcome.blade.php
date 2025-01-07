ce475ed2398406638723451836f0be773219a01d
felixjoshua <felixjoshua27@gmail.com>
2025-01-07 16:31:00 +0700

    Update tombol register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,7 +215,7 @@
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
                             class="relative px-3 md:px-5 py-2.5 bg-gradient-to-r from-orange-400 to-orange-300 rounded-full font-medium text-sm text-white hover:shadow-lg hover:shadow-orange-500/30 transition-all duration-300 ease-in-out hover:scale-105 animate-fade-in-right">
-                            Register
+                            Daftar
                         </a>
                     @endif
                 @endauth
