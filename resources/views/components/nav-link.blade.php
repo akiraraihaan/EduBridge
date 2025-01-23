6be7c74932504b7d545df2c93d0b772c60edb17f
Akira <raihanakirar@gmail.com>
2025-01-23 21:54:35 +0700

    Enhance navigation link components with improved styling and functionality
    - Updated the `nav-link` component to include an optional `isHome` prop, allowing for a home icon display when applicable.
    - Refactored the styling of both `nav-link` and `responsive-nav-link` components to use a consistent blue color scheme, enhancing visual coherence.
    - Improved hover effects and transition durations for a smoother user experience.
    
    These changes aim to provide a more engaging and visually appealing navigation experience across the application.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,21 @@
-@props(['active'])
+@props(['active', 'isHome' => false])
 
 @php
+$baseClasses = 'inline-flex items-center px-3 py-1.5 my-3 text-sm font-medium transition duration-300 ease-in-out rounded-md mx-1';
+
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+    ? $baseClasses . ' bg-blue-100 text-blue-600 hover:bg-blue-200'
+    : $baseClasses . ' text-gray-600 hover:bg-blue-50 hover:text-blue-500';
+
+$animation = 'hover:-translate-y-[0.5px]';
+$classes .= ' transform ' . $animation;
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
+    @if($isHome)
+        <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"/>
+        </svg>
+    @endif
     {{ $slot }}
 </a>
