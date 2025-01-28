6c54be5354d66a67a744ac8dcfe79d41ff15116b
Akira <raihanakirar@gmail.com>
2025-01-28 21:54:50 +0700

    add toml and modify vite config again

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,6 @@ export default defineConfig({
     build: {
         outDir: 'public/build',
         manifest: true,
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
     },
     server: {
         hmr: {
